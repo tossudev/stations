2585682de189f418490aff9583af545c9266b324
Ilkka Taneli Sirola <ilkka.sirola7@gmail.com>
2026-07-27 17:25:28 +0300

    graceful shutdown in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,65 @@
 package main
 
 import (
+	"context"
 	"os"
-
+	"os/signal"
 	"stations/internal"
+	"sync"
+	"syscall"
+	"time"
 )
 
-
 func main() {
-	mapfile, start, end, trainCount, ok := internal.ParseArgs()
-	if !ok {
-		ExitWithErrors()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	var wg sync.WaitGroup
+
+	wg.Go(func() {
+
+		mapfile, start, end, trainCount, ok := internal.ParseArgs()
+		if !ok {
+			ExitWithErrors()
+		}
+
+		contents := internal.ReadMapFile(mapfile)
+
+		graphList, ok := internal.ParseMap(string(contents), start, end)
+		if !ok {
+			ExitWithErrors()
+		}
+
+		_, paths := internal.MaxFlow(graphList, start, end)
+		internal.CreateSchedule(graphList, paths, start, end, trainCount, true)
+	})
+
+	done := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		internal.Log("Program exited successfully")
+	case <-ctx.Done():
+		internal.Log("Shutdown initiated")
+		shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer shutdownCancel()
+
+		select {
+		case <-done:
+			internal.Log("Program exited successfully")
+		case <-shutdownCtx.Done():
+			internal.Log("Shutdown timeout exceeded, forcing exit")
+			os.Exit(1)
+		}
 	}
 
-	contents := internal.ReadMapFile(mapfile)
-
-	graphList, ok := internal.ParseMap(string(contents), start, end)
-	if !ok {
-		ExitWithErrors()
-	}
-
-	_, paths := internal.MaxFlow(graphList, start, end)
-	internal.CreateSchedule(graphList, paths, start, end, trainCount, true)
-
-	internal.Log("Program exited successfully")
 }
-
 
 func ExitWithErrors() {
 	internal.PrintWarn("Program exited with errors")
-	os.Exit(0)
+	os.Exit(1)
 }
